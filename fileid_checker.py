18c8665e00ca43c1fcf605ab707d72cf5ec6e099
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2020-06-08 14:19:19 +0800

    Make bot works * Fix redis connectionn not close while exit program * Fix set special forward function not work

--- fileid_checker.py
+++ fileid_checker.py
@@ -19,7 +19,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 import asyncio
 import aiomysql
-from typing import List, NoReturn, Tuple, Dict, Union, Sequence
+from typing import List, Tuple, Dict, Union, Sequence
 from collections.abc import Iterable
 
 from pyrogram import Photo
@@ -37,11 +37,11 @@ class checkfile(MySqlDB):
 			charset: str='utf8mb4',
 			cursorclass: aiomysql.Cursor=aiomysql.DictCursor
 		):
-		self.lock = asyncio.Lock()
+		self.checker_lock = asyncio.Lock()
 		super().__init__(host, user, password, db, charset, cursorclass)
 
 	async def check(self, sql: str, exec_sql: str, args=()) -> bool:
-		async with self.lock:
+		async with self.checker_lock:
 			try:
 				if await self.query1(sql, args) is None:
 					await self.execute(exec_sql, args)
@@ -58,55 +58,83 @@ class checkfile(MySqlDB):
 	async def checkFile_dirty(self, file_id: str) -> bool:
 		return await self.query1("SELECT `id` FROM `file_id` WHERE `id` = %s", file_id) is None
 
-	async def insert_log(self, *args: Tuple[Tuple[str, ...], ...]) -> NoReturn:
+	async def insert_log(self, *args: Tuple[Tuple[str, ...], ...]) -> None:
 		await self.execute("INSERT INTO `msg_detail` (`to_chat`, `to_msg`, `from_chat`, `from_id`, `from_user`, `from_forward`) \
-			VALUES (%s, %s, %s, %s, %s, %s)", args)
+			VALUES (%s, %s, %s, %s, %s, %s)", args) # type: ignore
 
 	@staticmethod
 	def check_photo(photo: Photo) -> bool:
 		return not (photo.file_size / (photo.width * photo.height) * 1000 < checkfile.min_resolution or photo.file_size < 40960)
 
-	async def update_forward_target(self, chat_id: int, target: str) -> NoReturn:
+	async def update_forward_target(self, chat_id: int, target: str) -> None:
 		if await self.query1("SELECT * FROM `special_forward` WHERE `chat_id` = %s", chat_id):
-			await self.execute("UPDATE `special_forward` SET `target` = %s WHERE `chat_id` = %s", (target, chat_id))
+			await self.execute("UPDATE `special_forward` SET `target` = %s WHERE `chat_id` = %s", (target, chat_id)) # type: ignore
 		else:
-			await self.execute("INSERT INTO `special_forward` (`chat_id`, `target`) VALUE (%s, %s)", (chat_id, target))
+			await self.execute("INSERT INTO `special_forward` (`chat_id`, `target`) VALUE (%s, %s)", (chat_id, target)) # type: ignore
 
-	async def insert_blacklist(self, user_ids: Union[int, Sequence[int]]) -> NoReturn:
+	async def insert_blacklist(self, user_ids: Union[int, Sequence[int]]) -> None:
 		await self.execute("INSERT INTO `blacklist` (`id`) VALUES (%s)", user_ids, isinstance(user_ids, Iterable))
 
-	async def remove_blacklist(self, user_id: int) -> NoReturn:
+	async def remove_blacklist(self, user_id: int) -> None:
 		await self.execute("DELETE FROM `blacklist` WHERE `id` = %s", user_id)
 
 	async def query_user(self, user_id: int) -> Dict[str, Union[bool, int]]:
-		return await self.query1("SELECT * FROM `user_list` WHERE `id` = %s", user_id)
+		return await self.query1("SELECT * FROM `user_list` WHERE `id` = %s", user_id) # type: ignore
 
-	async def insert_bypass(self, user_id: int) -> NoReturn:
+	async def insert_bypass(self, user_id: int) -> None:
 		if await self.query_user(user_id):
 			await self.execute("UPDATE `user_list` SET `bypass` = 'Y' WHERE `id` = %s", user_id)
 		else:
 			await self.execute("INSERT INTO `user_list` (`id`, `bypass`) VALUE (%s, 'Y')", user_id)
 
-	async def insert_admin(self, user_id: int) -> NoReturn:
+	async def insert_admin(self, user_id: int) -> None:
 		if await self.query_user(user_id):
 			await self.execute("UPDATE `user_list` SET `authorized` = 'Y' WHERE `id` = %s", user_id)
 		else:
 			await self.execute("INSERT INTO `user_list` (`id`, `authorized`) VALUE (%s, 'Y')", user_id)
 
-	async def remove_admin(self, user_id: int) -> NoReturn:
+	async def remove_admin(self, user_id: int) -> None:
 		await self.execute("UPDATE `user_list` SET `authorized` = 'N' WHERE `id` = %s", user_id)
 	
 	async def query_all_admin(self) -> List[int]:
-		return [x['id'] for x in await self.query("SELECT `id` FROM `user_list` WHERE `authorized` = 'Y'")]
+		return [x['id'] for x in await self.query("SELECT `id` FROM `user_list` WHERE `authorized` = 'Y'")] # type: ignore
 
 	async def query_all_bypass(self) -> List[int]:
-		return [x['id'] for x in await self.query("SELECT `id` FROM `user_list` WHERE `bypass` = 'Y'")]
+		return [x['id'] for x in await self.query("SELECT `id` FROM `user_list` WHERE `bypass` = 'Y'")] # type: ignore
 	
 	async def query_all_blacklist(self) -> List[int]:
-		return [x['id'] for x in await self.query("SELECT `id` FROM `blacklist`")]
+		return [x['id'] for x in await self.query("SELECT `id` FROM `blacklist`")] # type: ignore
 
 	async def query_all_special_forward(self) -> Dict[int, int]:
-		return {x['chat_id']: x['target'] for x in await self.query("SELECT * FROM `special_forward`")}
+		return {x['chat_id']: x['target'] for x in await self.query("SELECT * FROM `special_forward`")} # type: ignore
 
 	async def query_forward_from(self, chat_id: int, message_id: int) -> Dict[str, int]:
-		return await self.query1("SELECT `from_chat`, `from_user`, `from_forward` FROM `msg_detail` WHERE `to_chat` = %s AND `to_msg` = %s", (chat_id, message_id))
+		return await self.query1("SELECT `from_chat`, `from_user`, `from_forward` FROM `msg_detail` WHERE `to_chat` = %s AND `to_msg` = %s", (chat_id, message_id)) # type: ignore
+
+	_instance = None
+	@classmethod
+	def get_instance(cls) -> 'checkfile':
+		if cls._instance is None:
+			raise RuntimeError('Instance not initialized')
+		return cls._instance
+
+	@classmethod
+	async def init_instance(cls, host: str, username: str, password: str, database: str) -> 'checkfile':
+		cls._instance = await cls.create(host, username, password, database)
+		return cls._instance
+
+	@classmethod
+	async def close_instance(cls) -> None:
+		await checkfile._instance.close()
+
+	@classmethod
+	async def create(cls,
+		host: str,
+		user: str,
+		password: str,
+		db: str,
+		charset: str='utf8mb4',
+		cursorclass: aiomysql.Cursor=aiomysql.DictCursor
+	) -> 'checkfile':
+		self = await super().create(host, user, password, db, charset, cursorclass)
+		return self # type: ignore
